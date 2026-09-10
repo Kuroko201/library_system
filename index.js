195becd52bf5cbdb2bdc0a28caea3720eed7dd84
Kuroko201 <matthewrainbowyt@gmail.com>
2026-09-10 23:28:01 +0800

    init, simple login and register without backend/css

---
+++ index.js
@@ -0,0 +1,28 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
