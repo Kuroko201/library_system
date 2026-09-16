014005d59cf617a9b98495b32e6d9eee764a17af
Kuroko201 <matthewrainbowyt@gmail.com>
2026-09-17 00:32:54 +0800

    update yaml and create table for progres

---
+++ migrations/0001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(255),
+    role VARCHAR(20) DEFAULT 'member',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
